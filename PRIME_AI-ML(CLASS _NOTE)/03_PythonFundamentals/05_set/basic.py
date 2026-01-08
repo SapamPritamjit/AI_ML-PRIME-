43e6dd2c192696bf82da825fddf574426aa17168
Sapam Pritamjit <pritamjitsapam@gmail.com>
2026-01-08 20:37:50 +0530

    set done

---
+++ PRIME_AI-ML(CLASS _NOTE)/03_PythonFundamentals/05_set/basic.py
@@ -0,0 +1,9 @@
+anb = {1, 2, 3, 45}
+
+# create an empty set
+# emp = {} # wrong
+emp = set() # correct
+
+print(type(emp))
+
+print(anb)
---
+++ PRIME_AI-ML(CLASS _NOTE)/03_PythonFundamentals/05_set/method.py
@@ -0,0 +1,23 @@
+s1 = {2, 43, 6, 7, 1, 32}
+
+s2 = {1, 2, 34, 54, 6,7}
+
+print(s1)
+
+s1.clear()
+print(s1)
+
+s1.add(5)
+print(s1)
+
+s1.remove(5)
+print(s1)
+
+s2.pop()
+print(s2)
+
+s1.add(5)
+
+print(s1.union(s2))
+
+print(s2.intersection(s2))
